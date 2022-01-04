0555f42eef5759ba662e721d1441f485b0feb664
yllkann <75842785+yllkann@users.noreply.github.com>
2022-01-04 18:56:49 +1100

    +

---
+++ tamplates.js
@@ -0,0 +1,35 @@
+
+
+
+let colors=["black","gray","rgb(44, 43, 43)"];
+
+let tamplates=document.getElementsByClassName(".tam");
+
+let i=0;
+
+let switcher=()=>
+{
+    tamplates=document.body.style.backgroundColor=colors[i];
+    i++;
+};
+setInterval(switcher,1000);
+
+
+document.getElementById("demo").innerHTML = "Choose you Design <br /> Zgjidhni dizajnin q&#1105 d&#1105shironi";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
